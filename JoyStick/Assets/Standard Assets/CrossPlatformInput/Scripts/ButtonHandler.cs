a5c19de140555eea07d687b5524f8112104ecb02
linxuan-yan <36588582+linxuan-yan@users.noreply.github.com>
2018-02-22 02:45:25 +0800

    Joystick
    JoyStick Sample Program

---
+++ JoyStick/Assets/Standard Assets/CrossPlatformInput/Scripts/ButtonHandler.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+    public class ButtonHandler : MonoBehaviour
+    {
+
+        public string Name;
+
+        void OnEnable()
+        {
+
+        }
+
+        public void SetDownState()
+        {
+            CrossPlatformInputManager.SetButtonDown(Name);
+            GameObject mycube = GameObject.Find("Cube");
+            Rigidbody r = mycube.GetComponent<Rigidbody>();
+            r.AddForce(new Vector3(0,15,0));
+        }
+
+
+        public void SetUpState()
+        {
+            CrossPlatformInputManager.SetButtonUp(Name);
+        }
+
+
+        public void SetAxisPositiveState()
+        {
+            CrossPlatformInputManager.SetAxisPositive(Name);
+        }
+
+
+        public void SetAxisNeutralState()
+        {
+            CrossPlatformInputManager.SetAxisZero(Name);
+        }
+
+
+        public void SetAxisNegativeState()
+        {
+            CrossPlatformInputManager.SetAxisNegative(Name);
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
